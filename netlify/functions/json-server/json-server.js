35a71b6cc13f0e5972c82ee5b36b24c53ca5f683
Shubham Yadav <yadavshubham0711@gmail.com>
2024-06-24 19:59:24 +0530

    Merge branch 'main' of https://github.com/ShubhamYadavGitHub/Angular-Assignment

---
+++ netlify/functions/json-server/json-server.js
@@ -0,0 +1,9 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('./netlify/functions/json-server/db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+module.exports = server;
