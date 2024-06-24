2a214f2124719da344ecd850b3a8a5f0a88a5a8f
Shubham Yadav <yadavshubham0711@gmail.com>
2024-06-24 20:15:00 +0530

    Fixed the issue "Runtime.HandlerNotFound - json-server.handler is undefined or not exported"

--- netlify/functions/json-server/index.js
+++ netlify/functions/json-server/index.js
@@ -1,3 +1,24 @@
+// const server = require('./json-server');
+
+// exports.handler = async (event, context) => {
+//   const response = await new Promise((resolve, reject) => {
+//     server(event, context, (error, result) => {
+//       if (error) {
+//         reject(error);
+//       } else {
+//         resolve(result);
+//       }
+//     });
+//   });
+
+//   return {
+//     statusCode: response.statusCode,
+//     headers: response.headers,
+//     body: response.body,
+//   };
+// };
+
+
 const server = require('./json-server');
 
 exports.handler = async (event, context) => {
@@ -12,8 +33,8 @@ exports.handler = async (event, context) => {
   });
 
   return {
-    statusCode: response.statusCode,
-    headers: response.headers,
-    body: response.body,
+    statusCode: response.statusCode || 200,
+    headers: response.headers || { 'Content-Type': 'application/json' },
+    body: response.body || '',
   };
 };
--- netlify/functions/json-server/json-server.js
+++ netlify/functions/json-server/json-server.js
@@ -1,3 +1,13 @@
+// const jsonServer = require('json-server');
+// const server = jsonServer.create();
+// const router = jsonServer.router('./netlify/functions/json-server/db.json');
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+// server.use(router);
+
+// module.exports = server;
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./netlify/functions/json-server/db.json');
